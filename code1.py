8ff9fbebbcfcd644c836bed938225cdec9b6ea0b
Narayan Pokhrel <56493136+npokhrel163@users.noreply.github.com>
2020-12-26 22:41:41 -0600

    Wrote the code

---
+++ code1.py
@@ -0,0 +1,26 @@
+
+from pylab import *
+import matplotlib.pyplot as plt
+def list_generator1():
+     time1 = linspace(0,5,50)
+     yi= int(input("what is the initial height of your trajectory? "))
+     g = 9.8
+     vi = int(input("What is the initial velocity of the particle? "))
+     list1 = []
+     for a in time1:
+        y_coordinate = yi + vi*a - (1/2)*g*(a**2)
+        list1.append(y_coordinate)
+     for heights in list1:
+        a = list1.index(heights)
+        if heights <0:
+            list1[a] = 0
+     
+     print(time1)
+     print(list1)
+     figure()
+     plot(time1, list1)
+     xlabel("time")
+     ylabel("height")
+     plt.show()
+
+list_generator1()
